0b62ec2ce44c4da1c3bddd845434ffeca12e19c1
Dina Alomari <71600389+denatotta@users.noreply.github.com>
2023-05-25 20:21:48 +0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const profileImage = document.getElementById('profile-image');
+    const images = ['dina.jfif', 't1.jfif', 't2.jfif', 't3.jfif']; // Add the filenames of the other images here
+    let currentIndex = 0;
+
+    function changeImage() {
+        profileImage.src = `images/${images[currentIndex]}`;
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    setInterval(changeImage, 3000); // Change image every 3 seconds
+});
